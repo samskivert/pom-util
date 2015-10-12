9aef90fdf7a8aca0c83eda0c5b32c94eb6e78a7b
Michael Bayne <mdb@samskivert.com>
2015-10-12 12:56:23 -0700

    Formatting tweak.

--- src/main/scala/pomutil/XMLUtil.scala
+++ src/main/scala/pomutil/XMLUtil.scala
@@ -10,8 +10,9 @@ import scala.xml.Node
  * Some XML utilities.
  */
 object XMLUtil {
+
   /** Obtains the text of the (first instance of the) specified child of the supplied node, if it
-   * exists. Trims whitespace along the way. */
+    * exists. Trims whitespace along the way. */
   def text (node :Node, child :String) :Option[String] =
     (node \ child).headOption map(_.text.trim)
 }
