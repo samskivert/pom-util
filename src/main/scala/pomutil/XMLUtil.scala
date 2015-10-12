238bc30e69721453920902cdee1a5ebf32827208
Michael Bayne <mdb@samskivert.com>
2015-10-12 13:05:05 -0700

    Provide a text() for all elems in a seq.

--- src/main/scala/pomutil/XMLUtil.scala
+++ src/main/scala/pomutil/XMLUtil.scala
@@ -4,15 +4,28 @@
 
 package pomutil
 
-import scala.xml.Node
+import scala.xml.NodeSeq
 
 /**
  * Some XML utilities.
  */
 object XMLUtil {
 
+  /** Returns the inner text of all elements in `node`. Generally this will be passed a node seq
+    * that contains the inside of one of Maven's standard "say what it is, then repeat what it is
+    * for every child node". For example if you had `modes` equal to the node:
+    * ```
+    * <modules>
+    *   <module>foo</module>
+    *   <module>bar</module>
+    *   <module>baz</module>
+    *   </modules>
+    * ```
+    * then you would call `text(mods \ "modules")` to get back `Seq("foo", "bar", "baz")`.
+    */
+  def text (node :NodeSeq) :Seq[String] = node map(_.text.trim)
+
   /** Obtains the text of the (first instance of the) specified child of the supplied node, if it
     * exists. Trims whitespace along the way. */
-  def text (node :Node, child :String) :Option[String] =
-    (node \ child).headOption map(_.text.trim)
+  def text (node :NodeSeq, child :String) :Option[String] = text(node).headOption
 }
